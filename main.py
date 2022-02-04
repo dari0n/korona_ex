c7c7a93ca9a4eca5bb20780907d5b9b6a5a224be
Ivan.Minin <79887423414@yandex.ru>
2022-02-04 11:10:59 +0300

    next push

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import pysftp
-import os.path
-import os
-import time
+import paramiko
 import datetime
-from pathlib import Path
+import time
+import os
+import shutil
+
+
+class Sftp_Connection(pysftp.Connection):
+    def __init__(self, *args, **kwargs):
+        try:
+            if kwargs.get('cnopts') is None:
+                kwargs['cnopts'] = pysftp.CnOpts()
+        except pysftp.HostKeysException as e:
+            self._init_error = True
+            raise paramiko.ssh_exception.SSHException(str(e))
+        else:
+            self._init_error = False
+
+        self._sftp_live = False
+        self._transport = None
+        super().__init__(*args, **kwargs)
+
+    def __del__(self):
+        if not self._init_error:
+            self.close()
 
 
 # Проверка наличия файла
@@ -11,89 +31,58 @@ def file_exist(path):
     return os.path.exists(path)
 
 
-def os_walk():
-    return os.walk('files')
-
-
-# Подключение по sftp
-def connection():
-    srv = pysftp.Connection('192.168.0.20', username='nt_exchange', private_key='keys/test_sftp',
-                            private_key_pass='9R1mapkl!@#')
-    return srv
-
-
 if __name__ == '__main__':
+    #Параметры
+    host = '192.168.0.20'
+    username = 'nt_exchange'
+    private_key = 'keys/test_sftp'
+    private_key_pass = ''
+    path_to_file = 'files/data.csv'
+    path_to_prepared_file = 'prepared_files/'
+    date_now = datetime.datetime.now()
+    file_prefix = date_now.strftime("%Y%m%d%H%M") #yyyymmdd24m
+    newFilename = 'ТК_' + file_prefix + '.csv.tmp' # Имя файла перед загрузкой
+    newFilenameOnServer = 'ТК_' + file_prefix + '.csv' # Имя файла после загрузки
+    path_to_upload = 'in/gds-ctl_mbm_catalog/'
+    path_to_download = 'out/gds-ctl_mbm_catalog/' # Возможно потом понадобится загружать ответы от удаленного сервера
+
+    #Цикл:
     while True:
-        PATH = 'files/data.csv'
-        preparedFilesPath = 'prepared_files/'
-        # print(type(connection()))
-        fex: bool = file_exist(PATH)
-
-        if fex == True:
-            date = datetime.datetime.now()
-            filenamePrefix = str(date.year) + str(date.month) + str(date.day) + str(date.hour) + str(date.minute) \
-                             + str(date.second)
-            filename = 'data.csv'
-            newFilename = filenamePrefix + '.csv.tmp'
-            newFilenameOnServer = filenamePrefix + '.csv'
-
-            print(fex)
-
-            print('Файл существует, ждем дозагрузку файла из 1С')
+        #Проверка наличия файла
+        if file_exist(path_to_file) == True:
+            print("Файл существует. Ждем дозагрузку из 1С")
             time.sleep(3)
 
+            # манипуляция с файлом
+            # Копирование файла
+            print("Копирование файла")
+            shutil.copy(path_to_file, path_to_prepared_file + newFilename)
 
-            # переместить файл # переименовать файл в tmp
-            Path('files/data.csv').rename(preparedFilesPath + newFilename)
-            print('Файл переименован в ' + newFilename + ' и перемещен в каталог отправки')
 
-            # коннект к серверу
+            # Коннект к серверу по ключу
+
+
             try:
-                srv = connection()
-                print(srv)
-                print('Соединение с sftp сервером установлено')
-            except:
-                print('Ошибка соединения')
+                with Sftp_Connection('192.168.0.20', username='nt_exchange', private_key='keys/test_sftp',
+                                     private_key_pass='9R1mapkl!@#') as sftp:
+                    # Лист директорий
+                    l = sftp.listdir()
+                    print(l)
+                    # Передача файла из prepared_files
+                    sftp.put(path_to_prepared_file + newFilename, path_to_upload + newFilenameOnServer)
+                    # Отключение от сервера
+                    print("Файл отправлен, отключение от сервера")
+                    sftp.__del__()
+                    print("Удаление старого файла: " + path_to_file)
+                    os.remove(path_to_file)
+                    print("Удаление временного файла: " + path_to_prepared_file + newFilename)
+                    os.remove(path_to_prepared_file + newFilename)
 
-            # отправить файл
-            try:
-                srv.put(preparedFilesPath + newFilename, newFilename)
-            except:
-                print('Ошибка отправки файла')
-            finally:
-
-                Path(preparedFilesPath + newFilename).unlink()
-                print('Файл отправлен')
-                print('Файл удален из каталога отправки')
-
-
-
-
-            # переименовать файл в data_datetimenow csv
-            try:
-                srv.rename(newFilename, newFilenameOnServer)
-            except:
-                print('Ошибка переименования файла')
-            finally:
-                print('Файл переименован в ' + newFilenameOnServer)
-
-
-
-
-            # дисконнект от сервера
-            if srv:
-                srv.close()
-                print('Соединение закрыто')
-            else:
-                print(srv)
-                print('Ошибка закрытия соединения')
-
-            # ждем 30 секунд
-
-            print('Ждем 30 секунд')
+            except paramiko.ssh_exception.SSHException as e:
+                print('Публичный ключ отсутствует в known_hosts', e)
 
         else:
-            print('Файл отсутствует, ждем 30 секунд')
-            time.sleep(3)
-    # for file in file_exist():
-    #   print(file)
+            print("Файл отсутствует ждем 30 секунд")
+            time.sleep(10)
+
+
