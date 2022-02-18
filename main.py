84da2d96845648701d503f3b87c370629d04a525
root <root@ex1.expertconsalt.local>
2022-02-18 16:42:48 +0000

    work

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import paramiko
 import pysftp
 import os
@@ -7,14 +8,13 @@ import shutil
 from base64 import decodebytes
 
 
-# Параметры
-filepath_directory = 'files/'
-path_to_prepared_file = 'prepared_files/'
+filepath_directory = '/home/nt_exchange/exchange/korona_ex/files/files/'
+path_to_prepared_file = '/home/nt_exchange/exchange/korona_ex/prepared_files/'
 prepared_file_type = '.tmp'
 host = 'gbd.ftc.ru'
 port = 11222
 username = 'cifrograd'
-private_key = 'keys/id_rsa'
+private_key = '/home/nt_exchange/exchange/korona_ex/keys/id_rsa'
 private_key_pass = 'f1n8sbOW6C38'
 key_data = b"""AAAAB3NzaC1yc2EAAAABIwAAAgEAzG68bROjTKziJkhxhFBwx4TOzb9oA9FvjalKXlYEqwgL4eHvpunaCMU7NKCSQXS5o/cBTDN5e+IBgkQhjAFsE/xliVl9q2HhT1ZV10gi2i0VbH4Qrp1bXLg0tpRv6CCDh6pz+te5lT881o07x47vkhtOPZVbDjuztCc9168F2CmKlriEH9ZhqTRWBtxaSC6t7ytTvNcvlUOGpJ0IGoAJggW9iY09AhdzU4Gt5FNMSPUkLf+DSwetxYJI5Tv3H63zUxYU1rhAkf374PbpiKc3hQiq/D3jtW7DEenqOHUPQUfCSXIadnQ87iKvaJ82/9tLyBCKHr9gReEPCc58c+pGklL8FZPbZfvxH+miOQTsXomjqdd4dhDiAERDS4srUPrD1F0GvfTU9rjpnhf3EmSjfAtLCRCJUMzpefn8Hl38ZYc/BlOY61Lz95XS0qx43yfO600rF1+iA0G5ve3PU//L9paTYe3sbb+wSzlIW36kq3OqzHPeZ7M9Z4a3OS/TfpM5CcnqZFSgbORKHt3n+6ukyubzJOGeELwMLgzUC+UnbrwuHecrWjmV3Z2O2jzwOZ1gO2RTt41JDMqaRPnmM+WeERKUYs3Op+8fkwcSPDVu9tEMWESbHyyWzoztPgTCHSvhA5XvbniLL0rDJNpIjSKYcBOmbJMi4M378Zk+b73fc7k="""
 key = paramiko.RSAKey(data=decodebytes(key_data))
@@ -58,7 +58,8 @@ if __name__ == '__main__':
         message = "[%s]"%(result)
         return message
 
-    # Смотри существуют ли файлы, если да, переименовываем в tmp и кладем prepared_files
+    """ Смотри существуют ли файлы, если да, переименовываем в tmp и кладем prepared_files """
+
     def prepare_exists(filepath_directory, path_to_prepared_file, file_type):
 
         for root, dirs, files in os.walk(filepath_directory):
@@ -66,8 +67,8 @@ if __name__ == '__main__':
                 for file in files:
                     fullpath = filepath_directory + file
                     shutil.copy(fullpath, path_to_prepared_file + file + file_type)
-                    # удаление файла
-                    os.remove(fullpath)
+                    # перемещение файла
+                    shutil.move(fullpath, '/home/nt_exchange/exchange/korona_ex/files/send/' + file)
                     print(current_time(), "Файл удален + " + fullpath)
                     return True
             else:
@@ -85,9 +86,9 @@ if __name__ == '__main__':
                                          private_key_pass=private_key_pass, cnopts=cnopts) as sftp:
                         # Лист директорий
                         l = sftp.listdir(remotepath=path_to_upload)
-                        print(l)
+                        print(current_time(), 'Текущее состояние каталога OUT' ,l)
                         for file in files:
-                            print(current_time(), path_to_upload + file)
+                            print(current_time(),'Загрузка файла', path_to_upload + file)
                             sftp.put(prepared_files + file, path_to_upload + file)
 
                             print(current_time(), 'Файл отправлен: ' + path_to_upload + file)
@@ -117,7 +118,7 @@ if __name__ == '__main__':
                                  private_key_pass=private_key_pass, cnopts=cnopts) as sftp:
                 # Лист директорий
                 l = sftp.listdir(remotepath=path)
-                print(l)
+                print(current_time()," Проверка каталога out", l)
 
         except paramiko.ssh_exception.SSHException as e:
             print(current_time(), 'Публичный ключ отсутствует в known_hosts', e)
@@ -136,7 +137,7 @@ if __name__ == '__main__':
                         pass
                     else:
                         print(current_time(), rfile)
-                        sftp.get(path + rfile, 'out/' + rfile)
+                        sftp.get(path + rfile, '/home/nt_exchange/exchange/korona_ex/files/out/' + rfile)
                         sftp.remove(path + rfile)
                 sftp.__del__()
                 return True
