744fdf5afe4eb94c5026f9558acb094cead5fbd2
Ivan Zabegalin <43495636+dari0n@users.noreply.github.com>
2024-05-24 04:22:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ from base64 import decodebytes
 filepath_directory = '/home/nt_exchange/exchange/korona_ex/files/files/'
 path_to_prepared_file = '/home/nt_exchange/exchange/korona_ex/prepared_files/'
 prepared_file_type = '.tmp'
-host = 'gbd.ftc.ru'
+host = ''
 port = 11222
-username = 'cifrograd'
+username = ''
 private_key = '/home/nt_exchange/exchange/korona_ex/keys/id_rsa'
-private_key_pass = 'f1n8sbOW6C38'
-key_data = b"""AAAAB3NzaC1yc2EAAAABIwAAAgEAzG68bROjTKziJkhxhFBwx4TOzb9oA9FvjalKXlYEqwgL4eHvpunaCMU7NKCSQXS5o/cBTDN5e+IBgkQhjAFsE/xliVl9q2HhT1ZV10gi2i0VbH4Qrp1bXLg0tpRv6CCDh6pz+te5lT881o07x47vkhtOPZVbDjuztCc9168F2CmKlriEH9ZhqTRWBtxaSC6t7ytTvNcvlUOGpJ0IGoAJggW9iY09AhdzU4Gt5FNMSPUkLf+DSwetxYJI5Tv3H63zUxYU1rhAkf374PbpiKc3hQiq/D3jtW7DEenqOHUPQUfCSXIadnQ87iKvaJ82/9tLyBCKHr9gReEPCc58c+pGklL8FZPbZfvxH+miOQTsXomjqdd4dhDiAERDS4srUPrD1F0GvfTU9rjpnhf3EmSjfAtLCRCJUMzpefn8Hl38ZYc/BlOY61Lz95XS0qx43yfO600rF1+iA0G5ve3PU//L9paTYe3sbb+wSzlIW36kq3OqzHPeZ7M9Z4a3OS/TfpM5CcnqZFSgbORKHt3n+6ukyubzJOGeELwMLgzUC+UnbrwuHecrWjmV3Z2O2jzwOZ1gO2RTt41JDMqaRPnmM+WeERKUYs3Op+8fkwcSPDVu9tEMWESbHyyWzoztPgTCHSvhA5XvbniLL0rDJNpIjSKYcBOmbJMi4M378Zk+b73fc7k="""
+private_key_pass = ''
+key_data = b"""AAAA"""
 key = paramiko.RSAKey(data=decodebytes(key_data))
 cnopts = pysftp.CnOpts()
 cnopts.hostkeys.add(host, 'ssh-rsa', key)
